1887b2233f6d5c89301ae1f5bc5489358779d8ca
YWTechIT <abcd8637@naver.com>
2021-08-18 14:55:25 +0900

    ADD interface Data

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,22 @@
+/* eslint-disable @typescript-eslint/no-unused-vars */
+import Container from "./components/container";
+import Card from "./components/molecules/card";
+import usePosts from "./hooks/useposts";
+
 const App = () => {
-  return(
-    <div>Hello!</div>
-  )
-}
+  const [data, loading, error ]= usePosts();
+
+  if (loading) {
+    return <div>loading...</div>;
+  }
+
+  return (
+    <Container>
+      {data.map((item) => (
+        <Card item={item}></Card>
+      ))}
+    </Container>
+  );
+};
 
 export default App;
---
+++ src/types/index.ts
@@ -0,0 +1,6 @@
+export interface Data {
+    userId: number;
+    id: number;
+    title: string;
+    body: string;
+}
